902c249ce245d68eafa0adf7a100e05500290541
Kyle <kyle.irving@my.jcu.edu.au>
2018-04-04 21:43:51 +1000

    handle_increment fixed

--- Prac 6/Conversion.py
+++ Prac 6/Conversion.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+MILES_TO_KM = 1.60934
+
 class Conversion(App):
     def build(self):
         Window.size = (400, 200)
@@ -9,13 +11,23 @@ class Conversion(App):
         self.root = Builder.load_file('Conversion.kv')
         return self.root
 
-    def handle_calculate(self, value):
-        result = value * 1.60934
+    def handle_calculate(self):
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
-    def handle_increment(self, value):
-        new_increment = value + 1
-        self.root.ids.output_label.text =str(new_increment)
+    def handle_increment(self, change):
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+
+    def get_validated_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
 
 
 
